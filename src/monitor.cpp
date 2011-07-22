440d243655479cff16ad4b595cdab26cf269348a
Taylor O'Brien <obrien.taylorm@gmail.com>
2011-07-22 13:42:17 -0500

    header formatting

--- src/monitor.cpp
+++ src/monitor.cpp
@@ -6,8 +6,8 @@
 * 	patients in bed.  It will send a notification when	a patient appears to	*
 * 	be in the process of getting out of bed.									*
 * 																				*
-* 	This code was created using examples and modified sample code from			*
-* 	OpenNI. Used by permission under the GNU Lesser General Public License.		*
+* 	This code was created using examples and modified sample code from OpenNI.	*
+* 	Used by permission under the GNU Lesser General Public License.				*
 * 																				*
 * 	Last Modified 21 July 2011 by Taylor O'Brien								*
 ********************************************************************************/
